da3c514f6de984ec4065d643a4d34daba1b31f03
Ezgi Hacihalil <eha@emakina.fr>
2020-02-24 18:29:29 +0300

    [Feature] Add types to fetcher function.

--- src/app/services/fetcher.ts
+++ src/app/services/fetcher.ts
@@ -5,57 +5,56 @@ interface Config {
   body?: string;
 }
 
-const fetcher = (method: string,
-  url: string,
-  data?: object | null,
-  headers: object = {}) => new Promise((resolve, reject) => {
-  const headerList = Object.assign(headers, {
-    'Content-Type': 'application/json;',
-    authorization: 'Bearer BQBeAkOcj-jYPuBqpCRtAVNFhIvi6tdY3Gpzeg-AuxQ_2mjb5wdGNL5gwPwZ7rFlKkictyqgPhyK44XLKcSMaaNP7SM-fjGUSUxxsu5jBOt8uZQSJplC3QMESQFw4DIX7Fs-8ntD9s3A4QsA6eorwG3nu7eLagANW_JEa7ZW8t6zn9X6dwoeT1ED0RdLxSMB84OJwk56MItgdF-nVSxIAeFBLDGevWzj1IDbtNIjHmRaRb5di_Tirsyxo1YqO2CPRSTwPYAgVMYM9w',
-  });
+function fetcher(method: string, url: string, data?: object | null, headers: object = {}): Promise<object> {
+  return new Promise((resolve, reject) => {
+    const headerList = Object.assign(headers, {
+      'Content-Type': 'application/json;',
+    });
 
-  const config: Config = {
-    method,
-    headers: new Headers(headerList),
-  };
+    const apiKey = 'SaFFFOLMtjkDiJuJxgmQSjEps8oLT8Wmf0i9lQxr';
+    const config: Config = {
+      method,
+      headers: new Headers(headerList),
+    };
 
-  if (method === 'POST' || method === 'PUT') {
-    config.body = JSON.stringify(data);
-  }
+    if (method === 'POST' || method === 'PUT') {
+      config.body = JSON.stringify(data);
+    }
 
-  if (method === 'DELETE') {
-    return fetch(url, config).then(
+    if (method === 'DELETE') {
+      return fetch(url, config).then(
+        (response) => {
+          if (response.ok) {
+            resolve({ headers: response.headers });
+          }
+        },
+      ).catch((err) => {
+        console.error('Error:', err);
+      });
+    }
+
+    return fetch(`${url}?api_key=${apiKey}`, config).then(
       (response) => {
         if (response.ok) {
-          resolve({ headers: response.headers });
+          response
+            .json()
+            .then((respJSON) => (respJSON.error
+              ? reject(respJSON) : resolve({ headers: response.headers, data: respJSON })));
+        } else {
+          response
+            .json()
+            .then((respJSON) => reject(respJSON));
         }
       },
     ).catch((err) => {
       console.error('Error:', err);
     });
-  }
-
-  return fetch(url, config).then(
-    (response) => {
-      if (response.ok) {
-        response
-          .json()
-          .then((respJSON) => (respJSON.error
-            ? reject(respJSON) : resolve({ headers: response.headers, data: respJSON })));
-      } else {
-        response
-          .json()
-          .then((respJSON) => reject(respJSON));
-      }
-    },
-  ).catch((err) => {
-    console.error('Error:', err);
   });
-});
+}
 
 export default {
-  get: (url: string, headers?: object) => fetcher('GET', url, null, headers),
-  post: (url: string, body?: object, headers?: object) => fetcher('POST', url, body, headers),
-  delete: (url: string, headers: object) => fetcher('DELETE', url, null, headers),
-  put: (url: string, body?: object, headers?: object) => fetcher('PUT', url, body, headers),
+  get: (url: string, headers?: object): Promise<object> => fetcher('GET', url, null, headers),
+  post: (url: string, body?: object, headers?: object): Promise<object> => fetcher('POST', url, body, headers),
+  delete: (url: string, headers: object): Promise<object> => fetcher('DELETE', url, null, headers),
+  put: (url: string, body?: object, headers?: object): Promise<object> => fetcher('PUT', url, body, headers),
 };
